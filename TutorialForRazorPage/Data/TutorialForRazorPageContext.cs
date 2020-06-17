3026f5ffc4e0912661dee0c6359445f4775c8ae6
ito0032 <ito.yoshihiro@ad-max.co.jp>
2020-06-17 17:48:35 +0900

    First commit

---
+++ TutorialForRazorPage/Data/TutorialForRazorPageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TutorialForRazorPage.Models;
+
+namespace TutorialForRazorPage.Data
+{
+    public class TutorialForRazorPageContext : DbContext
+    {
+        public TutorialForRazorPageContext (DbContextOptions<TutorialForRazorPageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TutorialForRazorPage.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ TutorialForRazorPage/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TutorialForRazorPage.Data;
+using TutorialForRazorPage.Models;
+
+namespace TutorialForRazorPage.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TutorialForRazorPage.Data.TutorialForRazorPageContext _context;
+
+        public CreateModel(TutorialForRazorPage.Data.TutorialForRazorPageContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
